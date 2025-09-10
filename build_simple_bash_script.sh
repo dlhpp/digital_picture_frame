39e0dd9df757572cccb2228ce70efe99297d3271
dlh <dlh@xps8300>
2025-09-10 13:49:24 -0700

    changed compiled files to have .exe extension

--- build_simple_bash_script.sh
+++ build_simple_bash_script.sh
@@ -1,21 +1,23 @@
 #!/bin/bash
 
-BINARY_NAME="exe.digital_picture_frame"
+BINARY_NAME="slideshow"
 PLATFORMS=("linux/amd64"  "linux/arm64"  "linux/arm/7"  "linux/arm/6"  "windows/amd64")
 
-GOOS=windows GOARCH=amd64         go build -o "${BINARY_NAME}_windows_amd64" .
-GOOS=linux   GOARCH=amd64         go build -o "${BINARY_NAME}_linux_amd64" .
-GOOS=linux   GOARCH=arm64         go build -o "${BINARY_NAME}_linux_arm64" .
+GOOS=linux   GOARCH=arm  GOARM=6  go build -o "${BINARY_NAME}_linux_armv6.exe" .
+GOOS=linux   GOARCH=arm  GOARM=7  go build -o "${BINARY_NAME}_linux_armv7.exe" .
 
-GOOS=linux   GOARCH=arm  GOARM=6  go build -o "${BINARY_NAME}_linux_armv6" .
-GOOS=linux   GOARCH=arm  GOARM=7  go build -o "${BINARY_NAME}_linux_armv7" .
+GOOS=linux   GOARCH=arm64         go build -o "${BINARY_NAME}_linux_arm64.exe" .
+GOOS=linux   GOARCH=amd64         go build -o "${BINARY_NAME}_linux_amd64.exe" .
+
+GOOS=windows GOARCH=amd64         go build -o "${BINARY_NAME}_windows_amd64.exe" .
+
 
 
 # Output files:
 # -----------------
-# exe.digital_picture_frame_linux_amd64
-# exe.digital_picture_frame_linux_arm64
-# exe.digital_picture_frame_linux_armv6
-# exe.digital_picture_frame_linux_armv7
-# exe.digital_picture_frame_windows_amd64.exe
+# slideshow_linux_armv6.exe
+# slideshow_linux_armv7.exe
+# slideshow_linux_arm64.exe
+# slideshow_linux_amd64.exe
+# slideshow_windows_amd64.exe
 
